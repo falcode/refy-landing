789446cc0f7ff8b8506381580ea6b5fefb6b87a1
falcode <sfalcon666@gmail.com>
2022-04-12 01:08:41 +0200

    minimum

---
+++ components/navbar.tsx
@@ -0,0 +1,29 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import logo from '../assets/svg/logo.svg'
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="fixed top-0 left-0 right-0 w-full bg-white flex h-16 pr-6 pl-6 flex justify-between">
+        <Image
+          src={logo}
+          alt="Refy Logo"></Image>
+        <div className="flex items-center justify-between space-x-9">
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          <Link href="/pricing">
+            <a>Pricing</a>
+          </Link>
+          <Link href="/contact">
+            <a>Contact</a>
+          </Link>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
