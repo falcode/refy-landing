59911d98fce2ad10df7d5768f3a24102d8ccb4c4
falcode <sfalcon666@gmail.com>
2022-11-05 17:50:21 +0100

    cookies banner

--- layout/layout.tsx
+++ layout/layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Footer from "../components/footer";
 import Navbar from "../components/navbar";
-import { ThemeProvider } from "@material-ui/core/styles";
+import { CookiesBanner } from "./cookies-banner";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -9,6 +9,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <Navbar></Navbar>
       <main className="z-10">{children}</main>
       <Footer></Footer>
+      <CookiesBanner />
     </>
   );
 }
